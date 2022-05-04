14bb8dea53b20098f19ea265095347df6d77ba0f
Mariusz Perzynski <m.perzyk@interia.pl>
2022-05-04 18:15:02 +0200

    upd.ex1/ex2

---
+++ ex1.cpp
@@ -0,0 +1,17 @@
+//Po przeanalizowaniu przykładów 1-3 przygotuj program obiektowy według poniższych instrukcji:
+//●stwórz klasę podstawowąOsobaoraz dwie klasy pochodneStudentorazPracownik
+//●klasy te powinny zawierać metodępokaz_dane()(przesłonięcie)
+//●utwórz w programie egzemplarze klasStudentorazPracownik,a następnie przypisz ichskładnikom danych wartości pobrane z klawiatury
+//Zadaniem programu jest wyświetlanie danych przechowywanych w stworzonychegzemplarzach klasStudentorazPracownik(przesłonięciemetodypokaz_dane()).
+
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <string>
+#include <stdlib.h>
+#include <iostream>
+#include <ctime>
+#include <dos.h>
+
+using namespace std;
+
---
+++ ex2.cpp
@@ -0,0 +1,3 @@
+//Zmodyfikuj program z zadania 1 tak,
+// aby przypisanie składnikom danych wartości pobranych zklawiatury, w stworzonych  egzemplarzach klas Student oraz Pracownik,
+// wykonane zostało zapomocą metody zaprzyjaźnionej z klasą.
