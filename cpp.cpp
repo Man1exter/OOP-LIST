76cd0607b95a86dea5bf4c9035e78a173ec2aa30
Mariusz Perzynski <m.perzyk@interia.pl>
2022-05-04 17:44:15 +0200

    3 class into the 1 main class

--- cpp.cpp
+++ cpp.cpp
@@ -2,3 +2,66 @@
 // ● stwórz klasę podstawowąOsobaoraz dwie klasy pochodneStudentorazPracownik
 // ● klasy te powinny zawierać metodępokaz_dane()(przesłonięcie)
 // ● utwórz w programie egzemplarze klasStudentorazPracownik,a następnie przypisz ichskładnikom danych wartości pobrane z klawiatury
+
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <string>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+class osoba
+{
+private:
+    string imie;
+    string nazwisko;
+    int wiek;
+
+protected:
+    int dane_osoby_wiek();
+
+public:
+    int pokaz_dane();
+
+     osoba()
+     {
+         imie = "MARIUSZ"
+         nazwisko = "PERZYNSKI"
+         wiek = 20;
+     }
+
+
+};
+
+int osoba::pokaz_dane_wiek()
+{
+    return wiek;
+}
+
+class pracownik:public osoba
+{
+private:
+    string stanowisko;
+public:
+    void pokaz_dane();
+
+    pracownik()
+    {
+        stanowisko = "LIDER";
+    }
+};
+
+class student:public osoba
+{
+private:
+    string grupa;
+public:
+    void pokaz_dane();
+
+    student()
+    {
+        grupa = "handlowcy"
+    }
+};
