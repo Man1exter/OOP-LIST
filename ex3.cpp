60d0dcde78592abb31f6d1d02ca07954074ff8d6
Mariusz Perzynski <m.perzyk@interia.pl>
2022-05-06 17:43:56 +0200

    mpc/2

--- ex3.cpp
+++ ex3.cpp
@@ -2,10 +2,14 @@
 // aby przypisanie składnikom danych wartości pobranych zklawiatury, w stworzonych  egzemplarzach klas Student oraz Pracownik,
 // wykonane zostało zapomocą metody zaprzyjaźnionej z klasą.
 
-#include <conio.h>
 #include <stdio.h>
+#include <conio.h>
+#include <math.h>
 #include <string.h>
-#include <ostream>
+#include <stdlib.h>
+#include <iostream>
+#include <ctime>
+#include <dos.h>
 
 using namespace std;
 
@@ -13,81 +17,46 @@ using namespace std;
 class osoba
 {
 protected:
-    string imie,nazwisko;
+    string imie;
+    string nazwisko;
     int wiek;
-    void poka_dane();
 };
-
-void osoba::poka_dane()
-{
-        printf("Początkowo imie = ", imie);
-        printf("Początkowo nazwisko= ", nazwisko);
-        printf("Początkowo wiek = ", wiek);
-}
 
 class osoba_dane:public osoba
 {
 private:
   void dane();
-  void osoba();
+  void pokaz_dane();
 public:
-  void process();
+void pokazanie();
 };
 
 void osoba_dane::dane()
 {
-cout << "podaj imie, nazwisko, wiek: " <<;
-cin >> imie,nazwisko,wiek;
+cout << "podaj imie, nazwisko, wiek: " << "\n";
+cin >> imie;
+cin >> nazwisko;
+cin >> wiek;
 }
 
-void osoba_dane::osoba()
+void osoba_dane::pokaz_dane()
 {
-cout << "Imie :" << imie << endl;
-cout << "Nazwisko :" << nazwisko << endl;
-cout << "Wiek :" << wiek << endl;
+cout << "imie = " << imie << "\n";
+cout << "nazwisko = " << nazwisko << "\n";
+cout << "wiek = " << wiek << "\n";
 }
 
-void osoba_dane::process()
-{
-dane();
-osoba();
-poka_dane();
-}
-
-class osoba_stanowisko:public osoba
-{
-private:
-void dane();
-void osoba();
-public:
-void process();
-};
-
-void osoba_stanowisko::dane()
-{
-cout << "Podaj stanowisko :" << "\n";
-cin >> stanowisko;
-}
-
-void osoba_stanowisko::osoba()
-{
-cout << "Stanowisko => " << stanowisko << "\n";
-}
-
-void osoba_stanowisko::process()
+void osoba_dane::pokazanie()
 {
 dane();
-osoba();
-poka_dane();
+pokaz_dane();
 }
+
 
 int main()
 {
-osoba_dane d1;
-d1.process();
-
-osoba_stanowisko d2;
-d2.process();
+    osoba_dane d1;
+    d1.pokazanie();
 
 return 0;
 }
